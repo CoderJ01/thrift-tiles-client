f20c979029b999b087307f80c548ff697ddcd109
CoderJ01 <93559542+CoderJ01@users.noreply.github.com>
2025-03-30 16:05:48 -0700

    change url

--- utils/url.js
+++ utils/url.js
@@ -1,3 +1,3 @@
 // export const frontendURL = 'http://127.0.0.1:5500';
-export const frontendURL = 'https://thrifttiles.com/';
+export const frontendURL = 'https://thrift-tiles-client.onrender.com';
 export const backendURL = 'https://thrift-tiles-store-server.onrender.com';
