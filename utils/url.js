5dd072014caf1c0f2c8e60d63a94aa355902e3f7
CoderJ01 <buildingguy@sbcglobal.net>
2025-03-30 18:00:00 -0700

    clean

--- utils/url.js
+++ utils/url.js
@@ -1,3 +1,5 @@
-// export const frontendURL = 'http://127.0.0.1:5500';
-export const frontendURL = 'https://thrift-tiles-client.onrender.com';
-export const backendURL = 'https://thrift-tiles-store-server.onrender.com';
+export const frontendURL = 'http://127.0.0.1:5500';
+// export const frontendURL = 'https://thrift-tiles-client.onrender.com';
+export const backendURL = 'http://localhost:3001';
+// export const backendURL = 'https://thrift-tiles-store-server.onrender.com';
+
