a9e31844f130f3c11ce8bccf03a9446659d2d4cc
CoderJ01 <buildingguy@sbcglobal.net>
2025-03-30 20:13:19 -0700

    edit

--- utils/url.js
+++ utils/url.js
@@ -1,5 +1,5 @@
-export const frontendURL = 'http://127.0.0.1:5500';
-// export const frontendURL = 'https://thrifttiles.com';
-export const backendURL = 'http://localhost:3001';
-// export const backendURL = 'https://thrift-tiles-store-server.onrender.com';
+// export const frontendURL = 'http://127.0.0.1:5500';
+export const frontendURL = 'https://thrifttiles.com';
+// export const backendURL = 'http://localhost:3001';
+export const backendURL = 'https://thrift-tiles-store-server.onrender.com';
 
